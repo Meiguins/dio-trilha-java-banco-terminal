36d69662e1ce5624efa873bc09a2dd1f61ce1357
Meiguins <kauanmeiguins3@gmail.com>
2024-04-24 20:57:41 -0300

    Adicionando variaveis

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Digite o número da Agência!");
+        sc.nextInt();
+
+        
+
+
+
+        sc.close();
     }
 }
