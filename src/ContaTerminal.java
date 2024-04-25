817774e9b6105b4bf7fd31ca5586762883dec323
Meiguins <kauanmeiguins3@gmail.com>
2024-04-24 21:33:08 -0300

    atualizando, ultimo commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -9,13 +9,19 @@ public class ContaTerminal {
         String nomeCliente;
         double saldo;
 
-        System.out.println("Digite o número da Agência!");
-        sc.nextInt();
+        System.out.println("Digite o número da Agência");
+        numero = sc.nextInt();
 
-        
+        System.out.println("Digite o nome da Agência!");
+        agencia = sc.next();
 
+        System.out.println("Por favor, diga seu nome!");
+        nomeCliente = sc.next();
 
+        System.out.println("Informe seu saldo");
+        saldo = sc.nextDouble();
 
+        System.out.println("Olá, " + nomeCliente + " ,obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " ,conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
         sc.close();
     }
 }
